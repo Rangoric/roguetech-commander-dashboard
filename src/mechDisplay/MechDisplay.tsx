85dd4b93bbf52f16b792fbec60afc6df12626aff
Wilson Gearld Mead III <kem@rangoric.com>
2023-01-31 23:47:59 -0500

    Some display things

--- src/mechDisplay/MechDisplay.tsx
+++ src/mechDisplay/MechDisplay.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, Grid } from "@mui/material";
+import React from "react";
 import { IMechAttackLog } from "../store/commanderAttackLog/IAttackLog";
 
 export interface IMechDisplayProps {
@@ -6,13 +8,23 @@ export interface IMechDisplayProps {
 
 export const MechDisplay = ({ mech }: IMechDisplayProps) => {
   return (
-    <div>
-      <div>{mech.name}</div>
-      <div>
-        {mech.weapons.map((t) => (
-          <div key={t.name + t.ammo + t.mode}>{t.name}</div>
-        ))}
-      </div>
-    </div>
+    <Grid container item spacing={1} xs={12}>
+      <Grid item xs={12}>
+        <Card variant={"elevation"}>
+          <CardContent>{mech.name}</CardContent>
+        </Card>
+      </Grid>
+      {mech.weapons.map((t) => (
+        <React.Fragment key={t.name + t.ammo + t.mode}>
+          <Grid item xs={6}>
+            <Card variant={"elevation"}>
+              <CardContent>
+                {t.name} - {t.ammo} - {t.mode}
+              </CardContent>
+            </Card>
+          </Grid>
+        </React.Fragment>
+      ))}
+    </Grid>
   );
 };
--- src/mechDisplay/MechsDisplay.tsx
+++ src/mechDisplay/MechsDisplay.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import { IMechAttackLog } from "../store/commanderAttackLog/IAttackLog";
 import { MechDisplay } from "./MechDisplay";
 
@@ -7,10 +8,10 @@ export interface IMechsDisplayProps {
 
 export const MechsDisplay = ({ mechs }: IMechsDisplayProps) => {
   return (
-    <>
+    <Grid container spacing={1}>
       {Object.keys(mechs).map((mechKey) => (
         <MechDisplay key={mechKey} mech={mechs[mechKey]} />
       ))}
-    </>
+    </Grid>
   );
 };
